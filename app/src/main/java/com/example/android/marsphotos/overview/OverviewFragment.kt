1cec6d90b36fe896bb3d5613be4f770c1c36a671
Adán Antonio Ramírez <60013835+AdanAntonio@users.noreply.github.com>
2023-04-12 00:03:38 -0600

    Add downloaded images to the recycler view using coil library

--- app/src/main/java/com/example/android/marsphotos/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/OverviewFragment.kt
@@ -36,9 +36,10 @@ class OverviewFragment : Fragment() {
      * to enable Data Binding to observe LiveData, and sets up the RecyclerView with an adapter.
      */
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
         val binding = FragmentOverviewBinding.inflate(inflater)
 
         // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
@@ -46,6 +47,8 @@ class OverviewFragment : Fragment() {
 
         // Giving the binding access to the OverviewViewModel
         binding.viewModel = viewModel
+
+        binding.photosGrid.adapter = PhotoGridAdapter()
 
         return binding.root
     }
